7126c4f6200c7c10920bb5f4772a51aef2a01297
Kishore_Tuggali <86900190+kishore445@users.noreply.github.com>
2024-05-07 20:02:17 +0530

    Banner code

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,57 @@
 import React from "react";
 import axios from "axios";
+import  { useState, useEffect } from 'react';
+import image1 from '../Asserts/1.jpg';
+import image2 from '../Asserts/2.jpg';
+import image3 from '../Asserts/3.jpg';
+import image4 from '../Asserts/4.jpg';
+import image5 from '../Asserts/5.jpg';
+import image6 from '../Asserts/6.jpg';
+import image7 from '../Asserts/7.jpg';
 
-const Banner = () => {
+const Banner = ({onprev,onNext}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [ image1,image2,image3,image4,image5,image6,image7]; 
+  const delay = 7000; 
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, delay);
+
+    return () => clearInterval(interval);
+  }, [images.length, delay]);
+  const loadNextPageMovies=()=>{
+    
+    if(currentIndex>=images.length-1 ){
+      setCurrentIndex(0);
+    }else{
+      setCurrentIndex((prevPage)=>prevPage+1);
+    }
+    
+  }
+  const loadPrevPageMovies=()=>{
+    if(currentIndex>0){
+      
+      setCurrentIndex((prevPage)=>prevPage-1);
+    }else{
+      setCurrentIndex(images.length - 1);
+    }
+    
+  }
   return (
-    <div className="h-[20vh] md:h-[45vh] bg-center bg-no-repeat flex items-end"
-      style={{
-        //  backgroundImage: `https://api.themoviedb.org/3/account/21229429/favorite/movies?language=en-US&page=1&sort_by=created_at.asc`,
-        backgroundImage: `url(https://media.themoviedb.org/t/p/w1920_and_h427_multi_faces/cnQI1M057kW0MHTmkQY4VeT6eCe.jpg&quot)`,
-      }}
-    >
-      <div className="text-xl md:text-3xl bg-gray-900 bg-opacity-60 p-4 w-full text-center text-white text-gray-50">Boy kills World</div>
+    <div className="h-[20vh] md:h-[55vh]  bg-repeat-x bg-center bg-cover "
+    style={{
+      backgroundImage: `url(${images[currentIndex]})`, 
+    }}
+  >
+      <div  >
+        <button onClick={loadPrevPageMovies}
+        className=" absolute left-0 top-1/4 m-4 p2  rounded-lg text-white font-bold text-8xl hover:scale-110 duration-100 py-10" >{"<"} </button>
+        
+        <button onClick={loadNextPageMovies}
+        className=" absolute right-0  top-1/4 m-4 p2 rounded-lg text-white font-bold text-8xl hover:scale-110 duration-100 py-10" >{">"}</button>
+    </div>
     </div>
   );
 };
