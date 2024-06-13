524bc9317008cbabfd317a668f183f4e66d5a85a
Kishore_Tuggali <86900190+kishore445@users.noreply.github.com>
2024-06-13 09:13:24 +0530

    Dev

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="flex space-x-8 items-center pl-3 py-4  bg-black">
         
-        <img src={logo} className="w-[80px] hover:scale-110 duration-100"/>
+        <img src={logo} alt="image" className="w-[80px] hover:scale-110 duration-100"/>
     
       <Link to="/"><h3 className="text-white hover:scale-110 duration-100 p-5">Movies</h3>  </Link>
       <Link to="/Watchlist"><h3 className="text-white hover:scale-110 duration-100 p-5">WatchList</h3></Link>
