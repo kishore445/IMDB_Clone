93c5832e2349c90542fd2e97bd498e67f8fdfd1e
Kishore_Tuggali <86900190+kishore445@users.noreply.github.com>
2024-06-15 21:58:46 +0530

    name

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -3,9 +3,9 @@ import React from 'react'
 const MovieInfo = ({movie}) => {
   const { title, overview, poster_path, release_date, vote_average } = movie;
   return (
-    <div className="bg-white rounded p-8">
+    <div className="bg-white rounded p-2  h-25">
         <img
-        className="w-full rounded-lg mb-4 h-[40vh]"
+        className="w-full rounded-lg mb-8 h-[40vh] flex"
         src={`https://image.tmdb.org/t/p/original/${poster_path}`}
         alt={`${title} Poster`}
       />
